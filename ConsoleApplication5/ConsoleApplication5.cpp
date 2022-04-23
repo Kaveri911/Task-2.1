965376b8fba1d68151f1adf281fb6b7e24af4749
Denis Senkov <defendercryak@gmail.com>
2022-04-23 20:16:38 +0300

    2 part

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -17,6 +17,43 @@ int mul(int a, int b)
 {
 return a * b;
 }
+int inv(int a)
+{
+	return a =-a;
+}
+void SortUbv(int* arr, int len) //сортировка по возрастанию массива
+{
+	int x = 0; // Доп. переменная для перестановки
+	for (int i = 0; i < len; i++)
+	{
+		for (int j = 1; j < len - i; j++)  // Цикл не доходит до конца, т.к. там элементы уже 
+		{                                    // стоят на своих местах
+			if (arr[j - 1] < arr[j])
+			{
+				x = arr[j - 1];
+				arr[j - 1] = arr[j];
+				arr[j] = x;
+			}
+		}
+	}
+}
+void SortVozr(int* arr, int len) //сортировка по возрастанию массива
+{
+	int x = 0; // Доп. переменная для перестановки
+	for (int i = 0; i < len; i++)
+	{
+		for (int j = 1; j < len - i; j++)  // Цикл не доходит до конца, т.к. там элементы уже 
+		{                                    // стоят на своих местах
+			if (arr[j - 1] > arr[j])
+			{
+				x = arr[j - 1];
+				arr[j - 1] = arr[j];
+				arr[j] = x;
+			}
+		}
+	}
+}
+
 void fold(int* arr, unsigned size, int (*fun)(int,int), int* acc) 
 {
 	unsigned i = 1;
@@ -34,23 +71,57 @@ void random_arr(int* arr, int len) // заполнение массива слу
 		arr[i] = rand() % 11 - 5; //заполнение числами в промежутке [-5;5]
 	}
 }
-
+void map(int* arr, unsigned size, int (*fun)(int)) {
+	unsigned i;
+	for (i = 0; i < size; i++) {
+		arr[i] = fun(arr[i]);
+		
+	}
+}
+void Print(int* arr, int len) //печать массива длины len
+{
+	for (int i = 0; i < len; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
  void main() {
  setlocale(LC_ALL, "rus");
  int a[10] = {};
  random_arr(a, 10);
  cout << "Сгенерированный случайный массив: ";
- for (int i = 0; i < 10; i++)
- {
-	 cout << a[i] << " ";
- }
-	 int result;
+ Print(a, 10);
+int result;
+fold(a, 10, maxx, &result);
+cout << "\nМаксимальный элемент = " << result;
+fold(a, 10, mul, &result);
+cout << "\nПроизведение элементов = " << result;
  fold(a, 10, sum, &result);
-cout << "\nСумма его элементов = " <<result;
- fold(a, 10, maxx, &result);
- cout << "\nМаксимальный элемент = " << result;
- fold(a, 10, mul, &result);
- cout << "\nПроизведение элементов = " << result;
+
+cout << "\nСумма его элементов = " <<result<<endl;
+if (result == a[0])
+{
+	map(a, 10, *inv);
+	cout << "CУММА = 1 эл. --> МАССИВ БЫЛ ИЗМЕНЕН!"<<endl;
+	cout << "Новый массив: "; 
+	Print(a, 10);
+}
+else if (result > a[0])
+{
+	SortVozr(a, 10);
+	cout << "CУММА > 1 эл. --> МАССИВ БЫЛ ИЗМЕНЕН!" << endl;
+	cout << "Новый массив: ";
+	Print(a, 10);
+}
+else
+{
+	SortUbv(a, 10);
+	cout << "CУММА < 1 эл. --> МАССИВ БЫЛ ИЗМЕНЕН!" << endl;
+	cout << "Новый массив: ";
+	Print(a, 10);
+}
+
  
 
 }
